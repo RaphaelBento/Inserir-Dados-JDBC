ad25729f722e1fa3172e336dcd5f2a80b6dc8cfd
RaphaelBento <rafhaesilvalbento@gmail.com>
2020-01-16 14:59:39 -0300

    terceiro commit adicionando arquivo

--- src/application/Program.java
+++ src/application/Program.java
@@ -12,31 +12,8 @@ public class Program {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Connection conn =null;
+		Preparede
 		
-		Connection conn = null;
-		Statement st =null;
-		ResultSet rs = null;
-		
-		try {
-			conn =DB.getConnection();
-			
-			st = conn.createStatement();
-			
-			rs = st.executeQuery("select * from coursejdbc.department");
-			
-			while(rs.next()) {
-				System.out.println(rs.getInt("Id") + " - " + rs.getString("Name"));
-			}
-		}
-		catch(SQLException e) {
-			e.printStackTrace();
-		}
-		finally {
-			DB.closeResultSet(rs);
-			DB.closeStatement(st);
-			DB.closeConnection();
-		}
 	}
-	
-
 }
