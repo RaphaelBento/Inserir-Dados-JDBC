504f38a444fcc13a647a807da42842b4f559b1ef
RaphaelBento <rafhaesilvalbento@gmail.com>
2020-01-16 12:03:02 -0300

    primeiro comitte

---
+++ src/application/Program.java
@@ -0,0 +1,19 @@
+package application;
+
+import java.sql.Connection;
+
+import db.DB;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		Connection conn = DB.getConnection();
+		
+		DB.closeConnection();
+		System.out.println(DB.getConnection());
+	}
+
+}
